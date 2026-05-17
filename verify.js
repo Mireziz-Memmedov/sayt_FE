542edaac68577c01f9578830a1ecbc2472f96ae6
Mireziz-Memmedov <azzmmmdv@gmail.com>
2026-05-17 08:47:38 +0000

    verify.js

--- verify.js
+++ verify.js
@@ -2,6 +2,8 @@ $(document).ready(function () {
 
     const inputs = document.querySelectorAll(".otp input");
 
+    // yazdiqda avtomatik olaraq novbeti xanaya kecmesi ve 
+    // tamalandiqda tesdiq olunmasi ucun funksiya.
     inputs.forEach((input, index) => {
         input.addEventListener("input", () => {
 
@@ -22,5 +24,39 @@ $(document).ready(function () {
         console.log("Verifying:", code);
         // fetch backend endpoint burada çağırılır
     }
+
+    // silende avtomatik olaraq novbeti xanaya kecmesi ucun funksiya.
+    inputs.forEach((input, index) => {
+        input.addEventListener("keydown", (e) => {
+
+            if (e.key === "Backspace" && index > 0 && input.value === "") {
+                inputs[index - 1].focus();
+            };
+
+        });
+    });
+
+    //saga ve sola xanalari istifade edende hereket etmesi ucun funksiya
+    inputs.forEach((input, index) => {
+        input.addEventListener("keydown", (e) => {
+
+            if (e.key === "ArrowLeft") {
+                if (index > 0) {
+                    inputs[index - 1].focus();
+                } else {
+                    inputs[inputs.length - 1].focus();
+                }
+            }
+
+            if (e.key === "ArrowRight") {
+                if (index < inputs.length - 1) {
+                    inputs[index + 1].focus();
+                } else {
+                    inputs[0].focus();
+                }
+            }
+
+        });
+    });
 
 });
